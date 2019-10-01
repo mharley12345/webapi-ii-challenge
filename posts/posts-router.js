5a97f65a4ad5e00a955490d73b32062fc96b5277
Mike Harley <mharley12345@gmail.com>
2019-10-01 14:21:07 -0700

    adds get by id

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -16,5 +16,21 @@ router.get('/',(req,res)=>{
         });
     })
 })
+router.get('/:id',(req,res)=> {
+    Posts.findById(req.params.id)
+          .then(post =>{
+              if(post){
+                  res.status(200).json(post)
+              } else {
+                  res.status(404).json({message:'User not found'})
+              } 
+         })
+         .catch(error =>{
+             console.log(error);
+             res.status(500).json({
+                message:'Server Error getting user'
+             })
+         })
+})
 
 module.exports = router
